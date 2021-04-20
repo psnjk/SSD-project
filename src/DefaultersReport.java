028e709c1e474aa7ef74281f794bbfde9798954c
TheSharpOwl <ziadxkabakibi@hotmail.com>
2021-04-20 21:54:21 +0300

    added 2 more classes

---
+++ src/DefaultersReport.java
@@ -0,0 +1,9 @@
+
+/// How would defaulters be detected?
+/// The DefaulterReportGenerator will traverse all events and
+/// for each student will calculate the number of points for each
+/// course. Then each course will be traversed and stu
+
+public abstract class DefaultersReport implements IReport{
+
+}
---
+++ src/DefaultersReportGenerator.java
@@ -0,0 +1,2 @@
+public abstract class DefaultersReportGenerator extends ReportGenerator{
+}
