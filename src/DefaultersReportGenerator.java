98dec92de52e29c4e065a8b531aa3bcbce2012b3
しずえ <cauchymoon@mail.ru>
2021-04-21 13:48:44 +0300

    Comments, consistency with UML...

--- src/DefaultersReportGenerator.java
+++ src/DefaultersReportGenerator.java
@@ -1,9 +1,14 @@
 
-/// How would defaulters be detected?
+/// How could defaulters be detected?
 /// The DefaulterReportGenerator will traverse all events and
 /// for each student will calculate the number of points for each
 /// course. Then each course will be traversed and students with
 /// points less than a threshold will be selected
 
 public abstract class DefaultersReportGenerator extends ReportGenerator{
+    @Override
+    protected IReport generateReport() {
+        // STUB
+        return null;
+    }
 }
--- src/StudentReportGenerator.java
+++ src/StudentReportGenerator.java
@@ -1,2 +1,7 @@
 public abstract class StudentReportGenerator extends ReportGenerator {
+    @Override
+    protected IReport generateReport() {
+        // STUB
+        return null;
+    }
 }
